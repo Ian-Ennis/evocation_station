5c90fd765e1993936a7ae2ff1d13509210f767cf
Ian <ipatrickenn7@gmail.com>
2022-05-06 12:05:49 -0600

    friday

--- src/Components/NewEvocation.js
+++ src/Components/NewEvocation.js
@@ -5,6 +5,39 @@ function NewEvocation() {
     function handleFileInput(e) {
         e.preventDefault();
 
+        // const file = e.target.image_upload.files[0]
+        const text = e.target.text.value
+        
+        // if ((file.size / 1000000) > 5) {
+        //     console.log('too big')
+        // } else {
+        //     console.log('good; size is under 5 mb')
+
+            const formData = new FormData();
+            formData.append("text", text);
+        //     formData.append("file", file);
+
+            // no content headers in post; they are implied as multipart/form-data by the 
+            // FormData object. Nor is it stringified.
+
+            fetch("http://localhost:3000/evocations", {
+                method: "POST", 
+                body: formData
+            })
+            .then((response) => {
+                console.log(response)
+                if (response.ok) {
+                  response.json().then(data => console.log(data))
+                } else {
+                    response.json()
+                    throw Error(response.status, response.statusText)
+                  }
+            })
+        }
+
+    function retrieveEvocations(e) {
+        e.preventDefault();
+        
         fetch("http://localhost:3000/evocations")
             .then((response) => {
                 console.log(response)
@@ -15,38 +48,6 @@ function NewEvocation() {
                     throw Error(response.status, response.statusText)
                   }
             })
-
-        // const file = e.target.image_upload.files[0]
-        // const text = e.target.text.value
-        
-        // if ((file.size / 1000000) > 5) {
-        //     console.log('too big')
-        // } else {
-        //     console.log('good; size is under 5 mb')
-
-        //     const formData = new FormData();
-        //     formData.append("file", file);
-        //     formData.append("text", text);
-
-            // no content headers in post; they are implied as multipart/form-data by the 
-            // Form Data object. Nor is it stringified.
-
-            // fetch("http://localhost:3000/evocations")
-            // .then((response) => {
-            //     console.log(response)
-            //     if (response.ok) {
-            //       response.json().then(data => console.log(data))
-            //     } else {
-            //         response.json()
-            //         throw Error(response.status, response.statusText)
-            //       }
-            // })
-        // }
-    }
-
-    function consoleLog(e) {
-        e.preventDefault();
-        console.log('hi')
     }
 
     return (
@@ -56,7 +57,7 @@ function NewEvocation() {
             <input type="text" name="text"></input>
             <button type="submit">Submit</button>
         </form>
-        <button onClick={consoleLog}>button</button>
+        <button onClick={retrieveEvocations}>retrieve</button>
         </>
     )
 }
