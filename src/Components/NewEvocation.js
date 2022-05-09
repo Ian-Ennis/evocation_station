980025d2fe58d60d3f024449a5334060d8422f78
Ian <ipatrickenn7@gmail.com>
2022-05-09 17:07:49 -0600

    image form data

--- src/Components/NewEvocation.js
+++ src/Components/NewEvocation.js
@@ -3,51 +3,57 @@ import React from "react";
 function NewEvocation() {
 
     function handleFileInput(e) {
-        e.preventDefault();
+      e.preventDefault();
 
-        // const file = e.target.image_upload.files[0]
-        const text = e.target.text.value
-        
-        // if ((file.size / 1000000) > 5) {
-        //     console.log('too big')
-        // } else {
-        //     console.log('good; size is under 5 mb')
+      const file = e.target.image_upload.files[0];
+      const text = e.target.text.value;
 
-            const formData = new FormData();
-            formData.append("text", text);
-        //     formData.append("file", file);
+      if (file.size / 1000000 > 5) {
+        console.log("too big");
+      } else {
+        console.log("good; size is under 5 mb");
 
-            // no content headers in post; they are implied as multipart/form-data by the 
-            // FormData object. Nor is it stringified.
+        const formData = new FormData();
+        formData.append("text", text);
+        formData.append("file", file);
 
-            fetch("http://localhost:3000/evocations", {
-                method: "POST", 
-                body: formData
-            })
-            .then((response) => {
-                console.log(response)
-                if (response.ok) {
-                  response.json().then(data => console.log(data))
-                } else {
-                    response.json()
-                    throw Error(response.status, response.statusText)
-                  }
-            })
-        }
+        // no content headers in post; they are implied as multipart/form-data by the
+        // FormData object. Nor is it stringified.
 
+        fetch("http://localhost:3000/evocations", {
+          method: "POST",
+          body: formData,
+        }).then(
+          fetch("http://localhost:3000/evocations").then((response) => {
+            console.log(response);
+            if (response.ok) {
+              response.json().then((data) => {
+                console.log(data);
+              });
+            } else {
+              response.json();
+              throw Error(response.status, response.statusText);
+            }
+          })
+        );
+      }
+    }
+
+    // turn this into a useEffect upon page load
     function retrieveEvocations(e) {
-        e.preventDefault();
-        
-        fetch("http://localhost:3000/evocations")
-            .then((response) => {
-                console.log(response)
-                if (response.ok) {
-                  response.json().then(data => console.log(data))
-                } else {
-                    response.json()
-                    throw Error(response.status, response.statusText)
-                  }
-            })
+      e.preventDefault();
+
+      fetch("http://localhost:3000/evocations").then((response) => {
+        console.log(response);
+        if (response.ok) {
+          response.json().then((data) => {
+            console.log(data);
+          });
+        } else {
+          response.json();
+          throw Error(response.status, response.statusText);
+        }
+      });
     }
 
     return (
