9c102fe0c88b2c02ac8272ae59a9f02c352dd83f
Ian <ipatrickenn7@gmail.com>
2022-05-11 14:09:18 -0600

    image and audio upload with render

--- src/Components/NewEvocation.js
+++ src/Components/NewEvocation.js
@@ -1,71 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid"
 
 function NewEvocation() {
+  const [posts, setPosts] = useState([]);
 
-    function handleFileInput(e) {
-      e.preventDefault();
+  const rootURL = `http://localhost:3000`;
 
-      const file = e.target.image_upload.files[0];
-      const text = e.target.text.value;
-
-      if (file.size / 1000000 > 5) {
-        console.log("too big");
+  useEffect(() => {
+    fetch("http://localhost:3000/evocations").then((response) => {
+      if (response.ok) {
+        response.json().then((data) => {
+          setPosts(data);
+          console.log('useEffect trigger')
+        });
       } else {
-        console.log("good; size is under 5 mb");
-
-        const formData = new FormData();
-        formData.append("text", text);
-        formData.append("file", file);
-
-        // no content headers in post; they are implied as multipart/form-data by the
-        // FormData object. Nor is it stringified.
-
-        fetch("http://localhost:3000/evocations", {
-          method: "POST",
-          body: formData,
-        }).then(
-          fetch("http://localhost:3000/evocations").then((response) => {
-            console.log(response);
-            if (response.ok) {
-              response.json().then((data) => {
-                console.log(data);
-              });
-            } else {
-              response.json();
-              throw Error(response.status, response.statusText);
-            }
-          })
-        );
+        response.json();
+        throw Error(response.status, response.statusText);
       }
-    }
+    })
+  }, [])
 
-    // turn this into a useEffect upon page load
-    function retrieveEvocations(e) {
-      e.preventDefault();
+  function handleFileInput(e) {
+    e.preventDefault();
 
+    const image = e.target.image_upload.files[0];
+    const audio = e.target.audio_upload.files[0];
+    const text = e.target.text.value;
+
+    const formData = new FormData();
+    formData.append("image", image);
+    formData.append("audio", audio);
+    formData.append("text", text);
+
+    fetch("http://localhost:3000/evocations", {
+      method: "POST",
+      body: formData
+    }).then(() => {
       fetch("http://localhost:3000/evocations").then((response) => {
         console.log(response);
         if (response.ok) {
           response.json().then((data) => {
-            console.log(data);
+            setPosts(data);
+            console.log('fetch completed')
+            console.log(posts)
           });
         } else {
           response.json();
           throw Error(response.status, response.statusText);
         }
-      });
-    }
+      })
+    });
+  }
 
+  const evocationData = posts.map(p => {
     return (
-        <>
-        <form id="new_evocation" onSubmit={handleFileInput}>
-            <input type="file" name="image_upload" accept="image/png, image/jpeg, image/jpg"></input>
-            <input type="text" name="text"></input>
-            <button type="submit">Submit</button>
-        </form>
-        <button onClick={retrieveEvocations}>retrieve</button>
-        </>
+      <div id="evocations" key={uuid().slice(0,8)}>
+        <p>{p.text}</p>
+        <img src={`${rootURL}${p.image}`}/>
+        <audio controls>
+          <source src={`${rootURL}${p.audio}`} type="audio/wav"/>
+        </audio>
+      </div>
     )
+  })
+
+  return (
+    <>
+      <form id="new_evocation" onSubmit={handleFileInput}>
+        <input
+          type="file" name="image_upload" accept="image/png, image/jpeg, image/jpg"></input>
+        <input type="file" name="audio_upload" accept="audio/*"></input>
+        <input type="text" name="text"></input>
+        <button type="submit">Submit</button>
+      </form>
+      {evocationData}
+    </>
+  );
 }
 
-export default NewEvocation
+export default NewEvocation;
